707a1a7ece305bd589e86b8db99ea21d092385ac
Slava Trofimov <my@pmb.link>
2023-07-15 12:15:04 -0400

    List table sorting

--- src/js/app.js
+++ src/js/app.js
@@ -70,7 +70,60 @@ async function initList() {
     })
     let tableBody = document.querySelector('#list table tbody')
     tableBody.innerHTML = tableContent
+}
 
+// sort products by title, description, price two way
+function sortProducts(sortBy,event) {
+    let sortType = event.target.dataset.sortType
+    console.log(sortType)
+    let sortedProducts = []
+    switch (sortBy) {
+        case 'title':
+            sortedProducts = products.sort((a, b) => {
+                if (a.title < b.title) {
+                    return -1
+                }
+                if (a.title > b.title) {
+                    return 1
+                }
+                return 0
+            })
+            break
+        case 'description':
+            sortedProducts = products.sort((a, b) => {
+                if (a.description < b.description) {
+                    return -1
+                }
+                if (a.description > b.description) {
+                    return 1
+                }
+                return 0
+            })
+            break
+        case 'price':
+            sortedProducts = products.sort((a, b) => {
+                if (a.price < b.price) {
+                    return -1
+                }
+                if (a.price > b.price) {
+                    return 1
+                }
+                return 0
+            })
+            break
+        default:
+            sortedProducts = products
+    }
+    
+    if (sortType === 'desc') {
+        sortedProducts = sortedProducts.reverse()
+        event.target.dataset.sortType = 'asc'
+    } else {
+        event.target.dataset.sortType = 'desc'
+    }
+
+    products = sortedProducts
+    initList()
 }
 
 // Path: './create'
